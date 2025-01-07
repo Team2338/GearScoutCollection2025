c0727b1b1c5fd15796bbd2ddb9de5ee9fd54ed21
Patrick Ubelhor <pubelhor@gmail.com>
2025-01-07 16:05:42 -0500

    Added Material UI

--- src/components/login-page/LoginPage.tsx
+++ src/components/login-page/LoginPage.tsx
@@ -1,5 +1,7 @@
 import './LoginPage.scss';
-import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import React, { useState } from 'react';
 import { IUser } from '../../model/Models.ts';
 
 interface IProps {
@@ -7,7 +9,26 @@ interface IProps {
 }
 
 export const LoginPage = (props: IProps) => {
+	const [username, setUsername] = useState<string>('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		props.handleLogin(null);
+	};
+
 	return (
-		<div>Login page</div>
+		<main className="page login-page">
+			<h1>Login page</h1>
+			<form onSubmit={ handleSubmit }>
+				<TextField
+					value={ username }
+					onChange={ (e) => setUsername(e.target.value) }
+					label="Username"
+					placeholder="Username"
+					name="username"
+				/>
+				<Button>Log in</Button>
+			</form>
+		</main>
 	);
 };
